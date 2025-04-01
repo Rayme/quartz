4167f75643a5ac8313363ca6ea756166820b45e0
RayLeigh <zm.rayme@gmail.com>
2025-04-01 14:13:46 +0800

    Quartz sync: Apr 1, 2025, 2:13 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "imzm.pages.dev",
+    baseUrl: "jefftay.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
