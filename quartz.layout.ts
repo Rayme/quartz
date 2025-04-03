75dd414336e722904b1a4ea85f3b70ac6c9ad69d
RayLeigh <zm.rayme@gmail.com>
2025-04-03 17:51:00 +0800

    edit layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,8 +42,8 @@ export const defaultContentPageLayout: PageLayout = {
 	}),
   ],
   right: [
-    Component.DesktopOnly(Component.TableOfContents()),
     Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
