97cf9615719a24d242cc3504fc14babb78592654
RayLeigh <zm.rayme@gmail.com>
2025-04-09 15:18:41 +0800

    changed config and layouts and css

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,9 +24,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Noto Sans Traditional Chinese",
-        body: "Source Sans Pro",
-        code: "Ubuntu Mono",
+        header: "IBM Plex Sans TC",
+        body: "Noto Serif SC",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,7 +3,19 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
+head: Component.Head({
+  meta: [],
+  links: [
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/@fontsource/noto-sans-sc/index.css",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/@fontsource/jetbrains-mono/index.css",
+    },
+  ],
+}),
   header: [],
   afterBody: [Component.RecentNotes({ title: "最近更新", limit: 4 })],
   footer: Component.Footer({
@@ -17,10 +29,6 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -50,7 +58,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
